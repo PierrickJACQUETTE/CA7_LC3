5785f02bbecb3a65115bf0c06aa7805c931d8cb0
RomainStasyszyn <romain.stasyszyn@gmail.com>
2016-12-11 14:57:49 +0100

    correction de commentaires dans le fichier testLEA1.asm

--- tests/ldst/testLEA1.asm
+++ tests/ldst/testLEA1.asm
@@ -1,7 +1,8 @@
 .ORIG x3000
 
-;chargement de la valeur 2 dans R0 via la constante definie l'adresse vaut 2.
-;resultat final : 0000 0000 0000 1000
+;chargement de la valeur 2 dans R0 via la constante definie a l'adresse 2 (ici 3002 car
+;le programme commence a l'adresse 3000).
+;resultat final : 0000 0000 0000 0010
 
 AND R0,R0,0
 LEA R0,const1
